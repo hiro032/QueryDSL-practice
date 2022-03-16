7ff9abc141ecfa54dd087afc556e5acc4f03a0fb
hiro <kiryuinh32@gmail.com>
2022-03-16 23:29:20 +0900

    QuerydslApplicationTest

--- src/test/java/stydy/querydsl/QuerydslApplicationTests.java
+++ src/test/java/stydy/querydsl/QuerydslApplicationTests.java
@@ -1,13 +1,37 @@
 package stydy.querydsl;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import stydy.querydsl.entity.Hello;
+import stydy.querydsl.entity.QHello;
+
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@Transactional
 class QuerydslApplicationTests {
+
+    @Autowired
+    EntityManager em;
 
     @Test
     void contextLoads() {
+        Hello hello = new Hello();
+        em.persist(hello);
+
+        JPAQueryFactory query = new JPAQueryFactory(em);
+        QHello qHello = new QHello("h");
+
+        Hello result = query
+                .selectFrom(qHello)
+                .fetchOne();
+
+        assertThat(hello).isEqualTo(result);
     }
 
 }
